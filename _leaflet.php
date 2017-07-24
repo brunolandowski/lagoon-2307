f321babce612f0019ba806f55306ba0755d577a1
brunolandowski <bruno.landowski@gmail.com>
2017-07-24 23:52:48 +0200

    'leaflet'

---
+++ _leaflet.php
@@ -0,0 +1,112 @@
+<?php 
+
+$pageCSS = '_leaflet.css';
+$className = 'leaflet';
+
+include "header.php";
+?>
+<div class="herobox">
+    <div class="title">
+        <h1>NOUVEAUTÉS</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis eros erat, in aliquam justo porta quis. Fusce feugiat sit amet magna egestas finibus.</p>
+    </div> 
+    <div class="pdfbox">
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
+    </div>
+</div>
+
+
+<div class="modules-list">
+    <div class="title">
+        <h2>SPECIFICATIONS</h2>
+    </div>
+    <div class="row">
+       <article>
+            <h3>380</h3>
+            <button>fr</button><!--
+            --><button>en</button>
+        </article>
+
+        <article>
+            <h3>39</h3>
+            <button>fr</button><!--
+            --><button>en</button>
+        </article>
+
+        <article>
+            <h3>400</h3>
+            <button>fr</button><!--
+            --><button>en</button>
+        </article>
+
+        <article>
+            <h3>42</h3>
+            <button>fr</button><!--
+            --><button>en</button>
+        </article>
+
+        <article>
+            <h3>450S</h3>
+            <button>fr</button><!--
+            --><button>en</button>
+        </article>
+
+        <article>
+            <h3>450F</h3>
+            <button>fr</button><!--
+            --><button>en</button>
+        </article>
+
+        <article>
+            <h3>52S</h3>
+            <button>fr</button><!--
+            --><button>en</button>
+        </article>
+
+        <article>
+            <h3>52F</h3>
+            <button>fr</button><!--
+            --><button>en</button>
+        </article>
+
+        <article>
+            <h3>560</h3>
+            <button>fr</button><!--
+            --><button>en</button>
+        </article>
+
+          <article>
+            <h3>620</h3>
+            <button>fr</button><!--
+            --><button>en</button>
+        </article>
+    </div>
+</div>
+
+<div class="pdfbox-2">
+    <div class="title">
+        <h2>BROCHURES</h2>
+    </div>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+
+</div>
+
+<?php 
+include "footer.php";
+?>
