b00248a542dbea33fca635d01ff1bf648a6148a3
brunolandowski <bruno.landowski@gmail.com>
2017-07-25 00:28:49 +0200

    'photo'

---
+++ _photo.php
@@ -0,0 +1,52 @@
+<?php 
+
+$pageCSS = '_photo.css';
+$className = 'photo';
+
+include "header.php";
+?>
+<div class="article-photo">
+    <div class="title">
+        <h1>CONDITIONS D'UTILISATION DES PHOTOS DE NICOLAS CLARIS</h1>
+       
+    </div> 
+
+    <div class="download-button">
+        <a href="">
+            <div class="iconpdf"></div>
+            <h5>CONDITIONS D'UTILISATION</h5>
+        </a>
+        <a href="">
+            <div class="iconpdf"></div>
+            <h5>CHARTE GRAPHIQUE</h5>
+        </a>
+    </div>
+
+    <div class="content">
+       <div class="row">
+            <h2>UTILISATION PAR LES DISTRIBUTEURS LAGOON</h2>
+            <article>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu lorem eu eros luctus dictum ut luctus lacus. Nunc sodales sodales nibh eu pharetra. Nunc at luctus diam. In sollicitudin nulla purus, quis dictum metus efficitur quis. Donec facilisis molestie diam, placerat luctus dui iaculis nec. Vivamus sed mi dolor. Donec eget vestibulum ex. Duis t, vestibulum mi. Pendisse ac ante eu tortor aliquam molestie. Suspendisse nec augue eget diam condimentum lacinia eu vitae urna. Praesent at mi ac metus aliquet imperdiet blandit ut ex. <br><br>Morbi pharetra orci lectus, tincidunt blandit ex pulvinar id. Pellentesque dictum et massa at condimentum. Sed euismod arcu a quam volutpat maximus. Donec eget sapien in odio posuere rhoncus id et arcu.</p>
+            </article>
+          
+       </div>
+        <div class="row">
+                <h2>UTILISATION PAR LES DISTRIBUTEURS LAGOON</h2>
+                <article>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu lorem eu eros luctus dictum ut luctus lacus. Nunc sodales sodales nibh eu pharetra. Nunc at luctus diam.<br><br>In sollicitudin nulla purus, quis dictum metus efficitur quis. Donec facilisis molestie diam, placerat luctus dui iaculis nec. Vivamus sed mi dolor. Donec eget vestibulum ex. Duis auctor rutrum tellus sit amet rhoncus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque lobortis, magna quis sagittis tincidunt, augue dolor luctus urna, congue convallis mi diam eu justo. Aliquam viverra odio gravida, porta est sit amet, vestibulum mi.Cras sed sa ut ex. Morbi pharetra orci lectus, tincidunt blandit ex pulvinar id. Pellentesque dictum et massa at condimentum. Sed euismod arcu a quam volutpat maximus. Donec eget sapien in odio posuere rhoncus id et arcu.</p>
+                </article>
+              
+           </div>
+        <div class="row">
+            <h2>UTILISATION PAR LES DISTRIBUTEURS LAGOON</h2>
+            <article>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu lorem eu eros luctus dictum ut luctus lacus. Nunc sodales sodales nibh eu pharetra. Nunc at luctus diam. In sollicitudin nulla purus, quis dictum metus efficitur quis. Donec facilisis molestie diam, placerat luctus dui iaculis nec. Vivamus sed mi dolor. Donec eget vestibulum ex. Duis auctor rutr lectus, tincidunt blandit ex pulvinar id. Pellentesque dictum et massa at condimentum. Sed euismod arcu a quam volutpat maximus. Donec eget sapien in odio posuere rhoncus id et arcu.</p>
+            </article>
+          
+       </div>
+    </div>
+</div>
+
+<?php 
+include "footer.php";
+?>
