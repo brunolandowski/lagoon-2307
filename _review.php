ad1e0700e366f3919ba630717a86a09bf9c7662d
brunolandowski <bruno.landowski@gmail.com>
2017-07-24 23:32:37 +0200

    'review'

---
+++ _review.php
@@ -0,0 +1,146 @@
+<?php 
+
+$pageCSS = '_review.css';
+$className = 'review';
+
+include "header.php";
+?>
+          
+<div class="lateral-list">
+    <div class="row">
+         <div class="title">
+            <h2>SAILS/VOILES</h2>
+        </div>
+       <article data-item="01">
+            <h3>380</h3>
+        </article>
+
+        <article data-item="01">
+            <h3>39</h3>
+        </article>
+
+        <article data-item="01">
+            <h3>400</h3>
+        </article>
+
+        <article data-item="01">
+            <h3>42</h3>
+        </article>
+
+        <article  data-item="01">
+            <h3>450S</h3>
+        </article>
+
+        <article data-item="01">
+            <h3>450F</h3>
+        </article>
+
+        <article data-item="01">
+            <h3>52S</h3>
+        </article>
+
+        <article data-item="01">
+            <h3>52F</h3>
+        </article>
+
+        <article data-item="01">
+            <h3>560</h3>
+        </article>
+
+          <article data-item="01">
+            <h3>620</h3>
+        </article>
+    </div>
+
+    <div class="row">
+         <div class="title">
+            <h2>LAGOON POWER</h2>
+        </div>
+       <article data-item="01">
+            <h3>630 MY</h3>
+        </article>
+
+        <article data-item="01">
+            <h3>40 MY</h3>
+        </article>
+
+        <article data-item="01">
+            <h3>POWER 43</h3>
+        </article>
+
+        <article data-item="01">
+            <h3>POWER 44</h3>
+        </article>
+    </div>
+
+    <div class="row">
+         <div class="title">
+            <h2>LAGOON</h2>
+        </div>
+       <article data-item="01">
+            <h3>CORPORATE</h3>
+        </article>
+       
+    </div>
+</div>
+
+<div class="content">
+    <div class="title">
+        <h1>SAILS/VOILES</h1>
+        <h2>450F</h2>
+    </div>
+    
+    <div class="table">
+        <table>
+            <tr class="label">
+                <th>NOM DU MAGAZINE</th>
+                <th>PAYS</th>
+                <th>DATES</th>
+                <th>TAILLE</th>
+                <th></th>
+              </tr>
+
+              <tr class="special">
+                <td class="name">MAGAZINE</td>
+                <td class="country">CHINE</td>
+                <td class="dates">AVRIL 2017</td>
+                <td class="size">1 Mo</td>
+                <td class="link"><span></span></td>
+              </tr>
+
+               <tr class="special">
+                <td class="name">MAGAZINE</td>
+                <td class="country">CHINE</td>
+                <td class="dates">AVRIL 2017</td>
+                <td class="size">1 Mo</td>
+                <td class="link"><span></span></td>
+              </tr>
+
+               <tr class="special">
+                <td class="name">MAGAZINE</td>
+                <td class="country">CHINE</td>
+                <td class="dates">AVRIL 2017</td>
+                <td class="size">1 Mo</td>
+                <td class="link"><span></span></td>
+              </tr>
+
+               <tr class="special">
+                <td class="name">MAGAZINE</td>
+                <td class="country">CHINE</td>
+                <td class="dates">AVRIL 2017</td>
+                <td class="size">1 Mo</td>
+                <td class="link"><span></span></td>
+              </tr>
+              
+
+            
+              
+            </table>
+    </div>
+</div>
+
+        
+  
+<?php 
+include "footer.php";
+?>
