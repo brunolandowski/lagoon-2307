ff5da44858eeb426803a6006134000d883d09e2a
brunolandowski <bruno.landowski@gmail.com>
2017-07-25 21:11:59 +0200

    'contact'

---
+++ _service.php
@@ -0,0 +1,168 @@
+<?php 
+
+$pageCSS = '_service.css';
+$className = 'service';
+
+include "header.php";
+?>
+<div class="herobox">
+    <div class="title">
+        <h1>CONTACT</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis eros erat, in aliquam justo porta quis. Fusce feugiat sit amet magna egestas finibus.</p>
+    </div> 
+</div>
+
+
+<div class="service-client">
+    <div class="row">
+        <article>
+            <h2>SERVICE CLIENT BORDEAUX</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dictum sodales pellentesque. Ut posuere rhoncus tellus ut dictum. Proin quis aliquam velit. Nam id lacus feugiat, malesuada augue quis, bibendum nulla. Quisque in elit a nibh efficitur varius eu et eros. Maecenas odio enim, posuere at porttitor vitae, volutpat in quam. Malesuada augue quis, bibendum nulla. Quisque in elit a nibh efficitur varius eu et eros. Maecenas odio enim, posuere at porttitor vitae, volutpat in quam.</p>
+              <div class="people">
+                <div class="name">LOÏC LAGRANGE</div>
+                <span class="function">RESPONSABLE COMMERCIAL</span>
+                 <div class="contacts">
+                    <div class="call">
+                        <div><b>Tel :</b> +15102362633</div>
+                        <div><b>Mob :</b> +15102362633</div>
+                        <div><b>Fax :</b> +15102362633</div>
+                        <div class="mail"><b>mail :</b> mail@mail.mail</div>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+           <div class="people">
+                <div class="name">LOÏC LAGRANGE</div>
+                <span class="function">RESPONSABLE COMMERCIAL</span>
+                 <div class="contacts">
+                    <div class="call">
+                        <div><b>Tel :</b> +15102362633</div>
+                        <div><b>Mob :</b> +15102362633</div>
+                        <div><b>Fax :</b> +15102362633</div>
+                        <div class="mail"><b>mail :</b> mail@mail.mail</div>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+        </article>
+
+        <article>
+           <h2>SERVICE CLIENT GIVRAND</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dictum sodales pellentesque. Ut posuere rhoncus tellus ut dictum.</p>
+             <div class="people">
+                <div class="name">LOÏC LAGRANGE</div>
+                <span class="function">RESPONSABLE COMMERCIAL</span>
+                 <div class="contacts">
+                    <div class="call">
+                        <div><b>Tel :</b> +15102362633</div>
+                        <div><b>Mob :</b> +15102362633</div>
+                        <div><b>Fax :</b> +15102362633</div>
+                        <div class="mail"><b>mail :</b> mail@mail.mail</div>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+             <div class="people">
+                <div class="name">LOÏC LAGRANGE</div>
+                <span class="function">RESPONSABLE COMMERCIAL</span>
+                 <div class="contacts">
+                    <div class="call">
+                        <div><b>Tel :</b> +15102362633</div>
+                        <div><b>Mob :</b> +15102362633</div>
+                        <div><b>Fax :</b> +15102362633</div>
+                        <div class="mail"><b>mail :</b> mail@mail.mail</div>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+             <div class="people">
+                <div class="name">LOÏC LAGRANGE</div>
+                <span class="function">RESPONSABLE COMMERCIAL</span>
+                 <div class="contacts">
+                    <div class="call">
+                        <div><b>Tel :</b> +15102362633</div>
+                        <div><b>Mob :</b> +15102362633</div>
+                        <div><b>Fax :</b> +15102362633</div>
+                        <div class="mail"><b>mail :</b> mail@mail.mail</div>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+        </article>     
+    </div>
+
+    <div class="row cta">
+         <article>
+            <h2>GARANTIES</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dictum sodales pellentesque. Ut posuere rhoncus tellus ut dictum. Proin quis aliquam velit. Nam id lacus feugiat, malesuada augue quis, bibendum nulla. Quisque in elit a nibh efficitur varius eu et eros. Maecenas odio enim, posuere at porttitor vitae, volutpat in quam.</p>
+            <button>LIRE LA SUITE</button>
+        </article>
+         <article>
+            <h2>OPTIONS/PIÈCES DÉTACHÉES</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dictum sodales pellentesque. Ut posuere rhoncus tellus ut dictum. Proin quis aliquam velit. Nam id lacus feugiat, malesuada augue quis, bibendum nulla. Quisque in elit a nibh efficitur varius eu et eros. Maecenas odio enim, posuere at porttitor vitae, volutpat in quam. Proin quis aliquam velit. Nam id lacus feugiat, malesuada augue quis, bibendum nulla. Quisque in elit a nibh efficitur varius eu et eros. Maecenas odio enim, posuere at porttitor vitae, volutpat in quam.</p>
+            <button>LIRE LA SUITE</button>
+        </article>
+    </div>
+</div>
+
+
+<div class="demandes">
+    
+     <div class="title">
+        <h2>CONTACT</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis eros erat, in aliquam justo porta quis. Fusce feugiat sit amet magna egestas finibus.</p>
+    </div> 
+    <div class="row">
+        <article>
+            
+           
+              <div class="people">
+              <h3>GAMME ACTUELLE</h3>
+                <div class="name">LOÏC LAGRANGE</div>
+                <span class="function">RESPONSABLE COMMERCIAL</span>
+                 <div class="contacts">
+                    <div class="call">
+                        <div><b>Tel :</b> +15102362633</div>
+                        <div><b>Mob :</b> +15102362633</div>
+                        <div><b>Fax :</b> +15102362633</div>
+                        <div class="mail"><b>mail :</b> mail@mail.mail</div>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+         
+        </article>
+
+        <article>
+           
+           
+             <div class="people">
+             <h3>ANCIENS MODÈLES</h3>
+                <div class="name">LOÏC LAGRANGE</div>
+                <span class="function">RESPONSABLE COMMERCIAL</span>
+                 <div class="contacts">
+                    <div class="call">
+                        <div><b>Tel :</b> +15102362633</div>
+                        <div><b>Mob :</b> +15102362633</div>
+                        <div><b>Fax :</b> +15102362633</div>
+                        <div class="mail"><b>mail :</b> mail@mail.mail</div>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+             
+             
+        </article>     
+    </div>
+</div>
+
+<?php 
+include "footer.php";
+?>
